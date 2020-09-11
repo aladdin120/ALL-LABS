dc98158f33a18749d463e52c4e1618c230a87921
Fedor <star1354@mail.ru>
2020-09-11 10:03:53 +0300

    Init

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Animal {
+}
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+}
