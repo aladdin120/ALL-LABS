6f32d7816a7f2253e5d86b8c53a0a299db7cabf5
Fedor <star1354@mail.ru>
2020-09-16 23:25:31 +0300

    Init

---
+++ src/LAB3/Book.java
@@ -0,0 +1,41 @@
+package LAB3;
+
+public class Book {
+    private String author;
+    private String name;
+    private int year;
+
+    public Book (String author, String name, int year) {
+        this.author = author;
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void getInfo() {
+        System.out.println("Автор: "+author+"\nНазвание книги: "+name+"\nГод написания: "+year);
+    }
+}
---
+++ src/LAB3/BookTest.java
@@ -0,0 +1,8 @@
+package LAB3;
+
+public class BookTest {
+    public static void main(String[] args) {
+        Book book = new Book("Дж. Оруэл", "1984", 1948);
+        book.getInfo();
+    }
+}
