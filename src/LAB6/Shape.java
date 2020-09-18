d56f6219bb69427c462e62dc42c0fd78e957e66a
Fedor <star1354@mail.ru>
2020-09-18 11:52:43 +0300

    Init

---
+++ src/LAB6/Shape.java
@@ -0,0 +1,23 @@
+package LAB6;
+
+public abstract class Shape {
+    private double S;
+    private double P;
+
+    public double getS() {
+        return S;
+    }
+
+    public void setS(double s) {
+        S = s;
+    }
+
+    public double getP() {
+        return P;
+    }
+
+    public void setP(double p) {
+        P = p;
+    }
+
+}
